f9edc4e5889733913d9aa8f3034b6da967153503
carlaac99 <cast4730@mylaurier.ca>
2021-03-19 19:02:27 -0400

    updated events class and CalData

--- src/sample/CalData.java
+++ src/sample/CalData.java
@@ -5,7 +5,7 @@ import java.util.ArrayList; // import the ArrayList class
 import java.util.Calendar;
 
 public class CalData{
-    public static ArrayList<Event> events = new ArrayList<Event>(); // Create an ArrayList object
+//    public static ArrayList<Event> events = new ArrayList<Event>(); // Create an ArrayList object
 
 
     public static void insertUser(String userName,String Pass,String firstName,String lastName,String email){
@@ -34,45 +34,7 @@ public class CalData{
 
     }
 
-    public static void getEvents(Integer userId){
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/calendar","root","");
 
-            String query="SELECT * FROM events WHERE userId=? ";
-
-            PreparedStatement preparedStmt = con.prepareStatement(query);
-            preparedStmt.setInt(1, userId);
-
-
-            ResultSet rs=preparedStmt.executeQuery();
-
-            if (!rs.next()){
-                System.out.println("No events");
-            }
-            else{
-                while(rs.next()!=false){
-
-                    String date=rs.getString("scheduledAt");
-                    int gr=rs.getInt("type");
-
-                    String title=rs.getString("title");
-
-                    Event e = new Event(title, date, true, "purple");
-
-                    events.add(e);
-                    System.out.println("done events");
-                }
-
-
-            }
-            con.close();
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
-
-    }
 
 
         public static void deleteEvent(Integer eventId){
@@ -171,6 +133,57 @@ public class CalData{
             System.out.println(e);
         }
     }
+    public static ArrayList<Event> getEvents(Integer userId){
+        ArrayList<Event> events = new ArrayList<Event>(); // Create an ArrayList object
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/calendar","root","");
+
+            String query="SELECT * FROM events WHERE userId=? ";
+
+            PreparedStatement preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, userId);
+
+
+            ResultSet rs=preparedStmt.executeQuery();
+
+//            if (!rs.next()){
+//                System.out.println("No events");
+//            }
+//            else{
+//
+            while(rs.next()!=false){
+
+                Timestamp date=rs.getTimestamp("scheduledAt");
+
+                String title=rs.getString("title");
+                Integer user_ID=rs.getInt("userId");
+                String description=rs.getString("description");
+                Integer ID=rs.getInt("id");
+                Integer type=rs.getInt("type");
+                String URL=rs.getString("url");
+                Timestamp triggeredAt=rs.getTimestamp("triggeredAt");
+
+
+                Event e = new Event(user_ID,title,date,type,description,URL,triggeredAt,ID);
+
+                events.add(e);
+                System.out.println("done events");
+            }
+
+
+//            }
+            con.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return events;
+
+
+    }
+
 
 //    public static void updateEvent(String title, Integer Id){
 //        try {
@@ -249,11 +262,11 @@ public class CalData{
         userLoginAuthentication("jeff_1234","mydog123");
         Timestamp date = new Timestamp(System.currentTimeMillis());
 //        insertEvent("dentist appointment","tommorow 2:30",date,"dsfdsfds","dsdsds",1,1,3,4);
-        getEvents(4);
+        ArrayList<Event> events=getEvents(4);
 
         System.out.println("For Loop");
         for (int counter = 0; counter < events.size(); counter++) {
-            System.out.println(events.get(counter).name);
+            System.out.println(events.get(counter).title);
         }
 
 
@@ -266,7 +279,7 @@ public class CalData{
         Timestamp timestamp = setEtime(year,month,day,hour,minute,second);
         Timestamp trigger = setTrigger(year,month,day,hour,minute,second);
 
-        insertEvent("test4", "test3", timestamp, trigger, "url", 1, 1);
+        insertEvent("test4", "test3", timestamp, trigger, "url", 1, 4);
 
     }
 
